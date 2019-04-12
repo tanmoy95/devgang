159f2423bd267591c91c694f198df0458a5059e7
Tanmoy Dutta Chowdhury <49547238+tanmoy95@users.noreply.github.com>
2019-04-12 11:24:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-//const cors=require('cors');
+const cors=require('cors');
 
 const{mongoose}=require('./db.js')
 var subjectController=require('./controllers/subjectController.js')
@@ -9,7 +9,7 @@ var questionController=require('./controllers/questionController.js')
 
 var app=express()
 app.use(bodyParser.json())
-//app.use(cors({origin:'http://localhost:4200'}));
+app.use(cors({origin:'http://localhost:4200'}));
 
 app.listen(3000,()=> console.log('Server started port at : 3000'))
 app.use('/subject',subjectController)
